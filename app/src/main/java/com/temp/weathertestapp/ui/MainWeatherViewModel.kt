99414fbd1de101b2824e5b42b35a9ac0fb04b7f9
Mihail Isaev <chumbam@mail.ru>
2022-05-23 15:11:55 +0300

    initial commit

---
+++ app/src/main/java/com/temp/weathertestapp/ui/MainWeatherViewModel.kt
@@ -0,0 +1,9 @@
+package com.temp.weathertestapp.ui
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainWeatherViewModel @Inject constructor(): ViewModel() {
+}
---
+++ app/src/main/java/com/temp/weathertestapp/ui/fragment/MoreWeatherFragment.kt
@@ -0,0 +1,10 @@
+package com.temp.weathertestapp.ui.fragment
+
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MoreWeatherFragment : Fragment() {
+
+}
